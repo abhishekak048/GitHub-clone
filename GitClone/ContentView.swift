4188ca93d1c89e1ed8938101439c5fed196d63fc
Abhishek kumar <54079908+abhishekak048@users.noreply.github.com>
2025-07-09 12:08:53 +0530

    Initial Commit

---
+++ GitClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitClone
+//
+//  Created by Abhishek Kumar on 09/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitClone/GitCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitCloneApp.swift
+//  GitClone
+//
+//  Created by Abhishek Kumar on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GitCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
